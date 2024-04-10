601f5f1539fc1e55f8594925a4711df5d3fc499b
Andile18 <zama.andile18@gmail.com>
2024-04-10 20:41:53 +0200

    Added select functionality

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ let istanbulTime = moment ().tz("Europe/Istanbul");
 istanbulDateElement.innerHTML =  moment().format ("DD MMMM YYYY");
 istanbulTimeElement.innerHTML = istanbulTime.format("HH: mm : ss[<small>] A [</small]");
 }, 1000);
+
+
+function updateCity(event){
+    let cityTimeZone = event.target.value;
+    let cityName =cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+ 
+ 
+     citiesElement.innerHTML = 
+     
+     `<div class="city">
+     <div>
+     <h2> ${cityName} </h2>
+     <div class="date"> ${cityTime.format("DD MMMM YYYY")} </div>
+     </div>
+     <div class="time"> ${cityTime.format("H:mm:ss")} <small> ${cityTime.format ("A")}</small> </div>
+     </div>`;
+     
+    
+ }
+ 
+ updateTime();
+ setInterval(updateTime, 1000);
+ 
+ 
+ let citiesSelectElement = document.querySelector("#city");
+ 
+ citiesSelectElement.addEventListener("change", updateCity);
+
+
+
