9eee6f2a42c808c8a47a30c60707930dcf7e4b0f
Andile18 <zama.andile18@gmail.com>
2024-04-10 21:00:25 +0200

    Updated Select functionality

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-setInterval(  function() { 
+function updateTime() { 
 
 let parisElement= document.querySelector ("#Paris");
+if (parisElement){ 
 let parisDateElement = parisElement.querySelector (".date");
 let parisTimeElement = parisElement.querySelector (".time");
 let parisTime = moment ().tz("Europe/Paris");
 
 parisDateElement.innerHTML =  moment().format ("DD MMMM YYYY");
-parisTimeElement.innerHTML = parisTime.format("HH: mm : ss [<small>] A [</small]");
+parisTimeElement.innerHTML = parisTime.format("HH: mm : ss [<small>] A [</small>]");
 
-}, 1000);
+}
 
-
-setInterval (function() { 
+ 
 let istanbulElement= document.querySelector ("#Istanbul");
+if (istanbulElement){
 let istanbulDateElement = istanbulElement.querySelector (".date");
 let istanbulTimeElement = istanbulElement.querySelector (".time");
 let istanbulTime = moment ().tz("Europe/Istanbul");
 
 istanbulDateElement.innerHTML =  moment().format ("DD MMMM YYYY");
-istanbulTimeElement.innerHTML = istanbulTime.format("HH: mm : ss[<small>] A [</small]");
-}, 1000);
+istanbulTimeElement.innerHTML = istanbulTime.format("HH: mm : ss[<small>] A [</small>]");
+
+}
+}
 
 
 function updateCity(event){
